26b0e0cc759cb2ef04db1575af12dbd40d4c11f0
Adam Orczyk <adam.orczyk@gmail.com>
2021-09-30 21:25:11 +0200

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,76 @@
-// Sprawdzić jak pakiety są wysyłane przez pilot
-// Ulepszyć funkcję sendPacket - dodać scheduler, tak jak przy mixed
-// Ujednolicić sendPacket i sendMixedPackets
+/**
+ * Power Functions Recorder.
+ * Records commands from LEGO Power Functions remote controls and plays them.
+ * 
+ * (c) 2021, Adam Orczyk
+ */
 
-basic.forever(function () {
-	
-})
+//% color=#f68420 icon="\uf111" block="PF Recorder"
+namespace pfRecorder {
+    let data: number[][] = [];
+    let isRecording = false;
+    let isPlaying = false;
 
-pfReceiver.connectIrReceiver(DigitalPin.P2)
-pfTransmitter.connectIrSenderLed(AnalogPin.P0)
-
-let data: number[][] = [];
-let isRecording = false;
-
-function startRecord(){
-    basic.clearScreen();
-    led.plot(0, 0)
-    serial.writeString('Recording...\n')
-    data = [];
-    pfReceiver.startRecord(data);
-}
-
-function stopRecord(){
-    // led.unplot(0, 0)
-    basic.showNumber(data.length);
-    pfReceiver.stopRecord();
-    serial.writeString(JSON.stringify(data) + '\n');
-}
-
-input.onButtonPressed(Button.A, function() {
-    if (isRecording) {
-        stopRecord();
-    } else {
-        startRecord()
+    function startRecord(){
+        basic.clearScreen();
+        led.plot(0, 0)
+        serial.writeString('Recording...\n')
+        data = [];
+        pfReceiver.startRecord([0], data);
     }
 
-    isRecording = !isRecording
-})
+    function stopRecord(){
+        basic.showNumber(data.length);
+        pfReceiver.stopRecord();
+        serial.writeString(JSON.stringify(data) + '\n');
+    }
 
-input.onButtonPressed(Button.B, function() {
-    basic.showString('>')
-    pfTransmitter.play(data);
-    basic.clearScreen()
-})
+    function onButtonA(){
+        if (isRecording) {
+            stopRecord();
+        } else {
+            startRecord()
+        }
+
+        isRecording = !isRecording
+    }
+
+    function onButtonB() {
+        if (!isPlaying) {
+            isPlaying = true;
+            control.runInBackground(() => {
+                basic.showString('>')
+                pfTransmitter.play(data);
+                basic.clearScreen()
+                isPlaying = false;
+            })
+        } else {
+            isPlaying = false;
+            pfTransmitter.stopPlaying();
+        }
+    }
+
+    input.onButtonPressed(Button.A, function() {
+        onButtonA()
+    })
+
+    input.onButtonPressed(Button.B, function() {
+        onButtonB()
+    })
+
+    /**
+     * Initialize recorder.
+     */
+    //% blockId="pfRecorder_init"
+    //% block="initialize : receiver pin %irReceiverPin transmitter pin %irTransmitterPin control channel %recorderControlChannel"
+    //% weight=100
+    export function init(irReceiverPin: DigitalPin, irTransmitterPin: AnalogPin, recorderControlChannel: PfReceiverChannel){
+        pfReceiver.connectIrReceiver(irReceiverPin)
+        pfTransmitter.connectIrSenderLed(irTransmitterPin)
+
+        pfReceiver.onRCcommand(recorderControlChannel, PfControl.Forward, PfControl.Float, PfAction.Pressed, onButtonA)
+        pfReceiver.onRCcommand(recorderControlChannel, PfControl.Float, PfControl.Forward, PfAction.Pressed, onButtonB)
+    }
+}
+
+pfRecorder.init(DigitalPin.P2, AnalogPin.P0, PfReceiverChannel.Channel2)
