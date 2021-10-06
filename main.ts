19f69dc7edb658360dfa976b0c29da2d0c4bcfba
Adam Orczyk <adam.orczyk@gmail.com>
2021-10-06 21:47:56 +0200

    Aktualizacja pxt.json, main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,78 +1,107 @@
-/**
- * Power Functions Recorder.
- * Records commands from LEGO Power Functions remote controls and plays them.
- * 
- * (c) 2021, Adam Orczyk
- */
-
-//% color=#f68420 icon="\uf111" block="PF Recorder"
 namespace pfRecorder {
     let data: number[][] = [];
     let isRecording = false;
     let isPlaying = false;
 
-    // type ReverseMap = {
-    //     [key: number]: number;
-    // };
-
-    // let reverseComboDirectMode: ReverseMap = {
-    //     // Combo direct mode
-    //     0b01: 0b10,
-    //     0b10: 0b01,
-    // }
-
-    // let reverseSingleOutputMode: ReverseMap = {
-    //     // Single output mode
-    //     0b0001: 0b1111,
-    //     0b0010: 0b1110,
-    //     0b0011: 0b1101,
-    //     0b0100: 0b1100,
-    //     0b0101: 0b1011,
-    //     0b0110: 0b1010,
-    //     0b0111: 0b1001,
-
-    //     0b1111: 0b0001,
-    //     0b1110: 0b0010,
-    //     0b1101: 0b0011,
-    //     0b1100: 0b0100,
-    //     0b1011: 0b0101,
-    //     0b1010: 0b0110,
-    //     0b1001: 0b0111,
-    // }
-
-    // Reversing commands (beta)
-    // ToDo: reverse only specified channel and output.
-    function reverseCommands(commands: number[][]): number[][]{
+    // Reversing commands order
+    export function reverseOrder(commands: number[][]): number[][] {
         let out: number[][] = [];
         let max = commands.length - 1;
 
-        for (let n = max; n >= 0; n--){
+        type Channel = {
+            [key: number]: number
+        }
+
+        let channels: Channel = {}
+
+        for (let n = max; n >= 0; n--) {
             let task = commands[n];
-
-            // // let channel = (0b001100000000 & task[0]) >>> 8;
-            // let mode = (0b000001110000 & task[0]) >>> 4;
-            // let data: number = 0;
-
-            // if (mode == 1) {
-            //     let red: number = (0b000000000011 & task[0]);
-            //     let blue: number = (0b000000001100 & task[0]) >>> 2;
-
-            //     data = (reverseComboDirectMode[blue] << 2) | reverseComboDirectMode[red];
-            // } else {
-            //     let output = (0b000000110000 & task[0]) >>> 4;
-
-            //     data = reverseSingleOutputMode[(0b000000001111 & task[0])];
-            // }
-
-            // let newTask: number = (0b111111110000 & task[0]) | data;
-            // out.push([newTask, task[1], task[2]])
             out.push(task)
+
+            if (!channels[task[1]]){
+                channels[task[1]] = 1;
+            }
+        }
+
+        for (let n of Object.keys(channels)){
+            out.push([0, +n, 1, 0, 0, 0b00010000])
+        }
+
+        serial.writeString('Reversed order:\n')
+        serial.writeString(JSON.stringify(out) + '\n');
+        return out;
+    }
+
+    // Reversing commands
+    // Reversing only commands from given channel and output.
+    export function reverseCommands(commands: number[][], channel: number, output: number): number[][]{
+        let out: number[][] = [];
+        let max = commands.length - 1;
+        type ReverseMap = {
+            [key: number]: number;
+        };
+
+        const reverseComboDirectMode: ReverseMap = {
+            1: 0b10,
+            2: 0b01,
+        }
+
+        const reverseSingleOutputMode: ReverseMap = {
+            1: 0b1111,
+            2: 0b1110,
+            3: 0b1101,
+            4: 0b1100,
+            5: 0b1011,
+            6: 0b1010,
+            7: 0b1001,
+
+            15: 0b0001,
+            14: 0b0010,
+            13: 0b0011,
+            12: 0b0100,
+            11: 0b0101,
+            10: 0b0110,
+            9: 0b0111,
+        }
+
+        for (let n = 0; n <= max; n++){
+            let task = commands[n];
+            let taskChannel = task[1];
+            let command = task[5];
+            let newCommand = task[5];
+            let mode = task[2];
+            let data = 0b00001111 & command;
+
+            if (taskChannel == channel){
+                if (mode == 1) {
+                    let red = task[3];
+                    let blue = task[4];
+
+                    if (output == 0 && reverseComboDirectMode[red]) {
+                        red = reverseComboDirectMode[red]
+                    } else if (output == 1 && reverseComboDirectMode[blue]) {
+                        blue = reverseComboDirectMode[blue]
+                    }
+
+                    data = (blue << 2) | red;
+                } else {
+                    let commandOutput = (0b00010000 & command) >>> 4;
+                    let newData = reverseSingleOutputMode[data];
+                    if (commandOutput == output && reverseSingleOutputMode[data]) {
+                        data = reverseSingleOutputMode[data];
+                    }
+                }
+
+                newCommand = (0b11110000 & command) | data;
+            }
+
+            out.push([task[0], task[1], task[2], task[3], task[4], newCommand])
         }
 
         // Last command is usually stop.
-        out.push(commands[max])
+        // out.push(commands[max])
 
-        serial.writeString('Reversed:\n')
+        serial.writeString('Reversed commands:\n')
         serial.writeString(JSON.stringify(out) + '\n');
         return out;
     }
@@ -120,8 +149,27 @@ namespace pfRecorder {
         if (!isPlaying) {
             isPlaying = true;
             control.runInBackground(() => {
+                basic.showString('*')
+                let reversed = reverseOrder(data);
                 basic.showString('<')
-                pfTransmitter.play(reverseCommands(data));
+                pfTransmitter.play(reversed);
+                basic.clearScreen()
+                isPlaying = false;
+            })
+        } else {
+            isPlaying = false;
+            pfTransmitter.stopPlaying();
+        }
+    }
+
+    function reverse2() {
+        if (!isPlaying) {
+            isPlaying = true;
+            control.runInBackground(() => {
+                basic.showString('*')
+                let reversed = reverseCommands(data, 0, 0);
+                basic.showString('<')
+                pfTransmitter.play(reversed);
                 basic.clearScreen()
                 isPlaying = false;
             })
@@ -159,8 +207,8 @@ namespace pfRecorder {
 
         pfReceiver.onRCcommand(recorderControlChannel, PfControl.Forward, PfControl.Float, PfAction.Pressed, onButtonA)
         pfReceiver.onRCcommand(recorderControlChannel, PfControl.Backward, PfControl.Float, PfAction.Pressed, onButtonB)
-        pfReceiver.onRCcommand(recorderControlChannel, PfControl.Float, PfControl.Backward, PfAction.Pressed, reverse)
+        pfReceiver.onRCcommand(recorderControlChannel, PfControl.Float, PfControl.Forward, PfAction.Pressed, reverse)
+        pfReceiver.onRCcommand(recorderControlChannel, PfControl.Float, PfControl.Backward, PfAction.Pressed, reverse2)
     }
 }
-
 pfRecorder.init(DigitalPin.P2, AnalogPin.P0, PfReceiverChannel.Channel2)
--- test.ts
+++ test.ts
@@ -1,1 +1,2 @@
-// tu są testy; nie będzie to kompilowane, gdy ten pakiet będzie używany jako rozszerzenie.
+
+serial.writeLine(JSON.stringify(pfRecorder.reverseCommands([[134, 0, 1, 1, 1, 0b0101], [108, 0, 6, 1, 1, 101]], 0, 0)))
